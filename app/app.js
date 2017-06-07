14c8c2650267911dc8c029aa338b8c0843bdd5c5
Timofey Ivaykin <ivaykin@live.com>
2017-06-07 21:29:38 +0300

    Changes for interview

--- app/app.js
+++ app/app.js
@@ -7,7 +7,47 @@ import _ from 'lodash'; // If you want underscore/lodash.
 import styles from './styles/app.scss';
 
 function buildAd() {
-    //JSON: http://live.spongecell.com/interview/ad_revision.json
+    $.get('http://live.spongecell.com/interview/ad_revision.json', function(data) {
+        var adScreen = $('<div id="ad-screen"/>');
+        adScreen.css({
+            height: data.size.height,
+            width: data.size.width,
+            'max-height': data.size.height,
+            'max-width': data.size.width
+        });
+        $('#main-container').append(adScreen);
+        function createElement(elementData) {
+            var domElement = $('<div/>');
+            domElement.css({
+                    background: "red",
+                    height: elementData.size.height,
+                    width: elementData.size.width,
+                    position: 'fixed',
+                    left: (elementData.position.left * data.size.width) + 'px',
+                    top: (elementData.position.top * data.size.height) + 'px'
+            });
+            return domElement;
+        }
+        function configureElement(elementSettings, domElement) {
+            if(elementSettings.type === 'image') {
+                domElement.css({
+                    background: "url('" + data.assets[elementSettings.image.masterAssetId] + "')",
+                })
+            } else {
+                domElement.append(elementSettings.text.text);
+            }
+            domElement.css({
+                'background-size': elementSettings.size.width + 'px ' + elementSettings.size.height + 'px'
+            });
+        }
+        data.settings.screens[0].elements.map(function(elementSettings) {
+            var domElement = createElement(elementSettings);
+            adScreen.append(domElement);
+            configureElement(elementSettings, domElement);
+        });
+    });
 }
 
-document.addEventListener('DOMContentLoaded', buildAd);
+$(document).ready(function() {
+    buildAd();
+});
